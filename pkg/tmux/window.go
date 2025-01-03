249e37931d812f532326dc5dc1e3d36649cc1bc8
pvskp <pvincius14@gmail.com>
2025-01-02 23:33:01 -0300

    feat(window): Adds functionalities to Window object

--- pkg/tmux/window.go
+++ pkg/tmux/window.go
@@ -1,5 +1,64 @@
 package tmux
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
 type Window struct {
-  Panes []Pane
+	Index       int
+	SessionName string
+	Name        string
+	Panes       map[string]*Pane
+}
+
+func (w *Window) kill() error {
+	cmd := &TmuxCommand{
+		Command: "kill-window",
+		Args: []string{
+			"-t",
+			fmt.Sprintf("%s:%d", w.SessionName, w.Index),
+		},
+	}
+
+  if _, err := cmd.Execute(); err != nil {
+    return err
+  }
+
+  return nil
+}
+
+func (w *Window) fetchPanes() {
+	cmd := &TmuxCommand{
+		Command: "list-panes",
+		Args: []string{
+			"-t",
+			fmt.Sprintf("%s:%d", w.SessionName, w.Index),
+			"-F",
+			"#P",
+		},
+	}
+
+	cmdOutput, err := cmd.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, pane := range strings.Split(cmdOutput, "\n") {
+		if pane != "" {
+			paneIndex, err := strconv.Atoi(pane)
+			if err != nil {
+				log.Fatal("Failed to convert pane str to int")
+			}
+
+			w.Panes = append(w.Panes, &Pane{
+				Index:       paneIndex,
+				WindowName:  w.Name,
+				SessionName: w.SessionName,
+			})
+		}
+	}
+
 }
